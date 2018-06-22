48c5557d0befa560b516533c72b2416a80f4d448
Wes Reid <bwreid@gmail.com>
2018-06-22 15:22:23 -0700

    Try updating migrations to handle potential column existing

--- src/db/migrations/20180622140225_cascade_task_deletion_from_list_deletion.js
+++ src/db/migrations/20180622140225_cascade_task_deletion_from_list_deletion.js
@@ -1,11 +1,11 @@
 exports.up = knex => {
   return knex.schema.alterTable('tasks', async table => {
     const hasColumn = await knex.schema.hasColumn('list_id')
-    if (hasColumn) table.dropColumn('list_id')
-  }).then(() => knex.schema.alterTable('tasks', table => {
+    if (hasColumn) await table.dropColumn('list_id')
+
     table.integer('list_id')
     table.foreign('list_id').references('lists.id').onDelete('CASCADE')
-  }))
+  })
 }
 
 exports.down = knex => {
