7423e19c7081588494a5f8dc7f4bfe233ead61c4
Wes Reid <bwreid@gmail.com>
2018-06-22 13:18:26 -0700

    Validate that users cannot update tasks that are not theirs

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -1,5 +1,6 @@
 const { SECRET_KEY } = process.env
 const { sign, verify } = require('jsonwebtoken')
+const db = require('../db')
 
 function createToken (id) {
   const sub = { sub: { id } }
@@ -25,8 +26,40 @@ function isLoggedIn (req, res, next) {
   }
 }
 
+async function isAuthorizedToUpdateTask (req, res, next) {
+  try {
+    const authorization = req.headers.authorization
+    if (!authorization) {
+      const message = `You are not authorized to access this route`
+      next({ status: 401, error: message })
+    }
+
+    const token = parseToken(authorization)
+    const userId = token.sub.id
+
+    const taskId = req.params.id
+    const task = await db('tasks').where({ id: taskId }).first()
+    if (!task) return next({ status: 404, error: `Could not find task with ID of ${taskId}` })
+
+    const list = await db('lists').where({ id: task.list_id }).first()
+    console.log(task, userId)
+    if (list.user_id !== userId) {
+      const message = `You are not authorized to update this list`
+      return next({ status: 401, error: message })
+    }
+
+    next()
+  } catch (e) {
+    next({
+      status: 401,
+      error: `Session has expired. Please login again.`
+    })
+  }
+}
+
 module.exports = {
   createToken,
   parseToken,
-  isLoggedIn
+  isLoggedIn,
+  isAuthorizedToUpdateTask
 }
--- src/routes/tasks.js
+++ src/routes/tasks.js
@@ -1,8 +1,9 @@
 const router = require('express').Router()
 const ctrl = require('../controllers/tasks')
+const auth = require('../lib/auth')
 
-router.post('/', ctrl.create)
-router.patch('/:id', ctrl.patch)
-router.delete('/:id', ctrl.destroy)
+router.post('/', auth.isAuthorizedToUpdateTask, ctrl.create)
+router.patch('/:id', auth.isAuthorizedToUpdateTask, ctrl.patch)
+router.delete('/:id', auth.isAuthorizedToUpdateTask, ctrl.destroy)
 
 module.exports = router
