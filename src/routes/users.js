2ec90873d1d2935c228fe7e36ac0c7f941994b1d
Wes Reid <bwreid@gmail.com>
2018-07-05 10:23:36 -0700

    Remove profile route

--- src/routes/users.js
+++ src/routes/users.js
@@ -3,6 +3,5 @@ const ctrl = require('../controllers/users')
 
 router.post('/signup', ctrl.signup)
 router.post('/login', ctrl.login)
-router.get('/profile', ctrl.isLoggedIn)
 
 module.exports = router
