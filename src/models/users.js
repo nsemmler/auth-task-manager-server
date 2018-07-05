ba12a4d3dc9aef503e5158e9789635824871a8e9
Wes Reid <bwreid@gmail.com>
2018-07-05 10:21:46 -0700

    Asyncify bcrypt

--- src/models/users.js
+++ src/models/users.js
@@ -1,8 +1,9 @@
+const { promisify } = require('util')
 const db = require('../db')
 const bcrypt = require('bcryptjs')
 
-function create ({ password, ...body }) {
-  const hashed = bcrypt.hashSync(password)
+async function create ({ password, ...body }) {
+  const hashed = await promisify(bcrypt.hash)(password, 8)
   return db('users')
     .insert({ ...body, password: hashed })
     .returning('*')
@@ -10,12 +11,13 @@ function create ({ password, ...body }) {
 }
 
 function login ({ email, password }) {
+  console.log('Logging in...')
   return db('users')
     .where({ email })
-    .then(([ user ]) => {
+    .then(async ([ user ]) => {
       if (!user) throw new Error()
 
-      const isValid = bcrypt.compareSync(password, user.password)
+      const isValid = await promisify(bcrypt.compare)(password, user.password)
       if (!isValid) throw new Error()
 
       return user
